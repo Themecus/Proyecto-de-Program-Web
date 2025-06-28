b6ace976c604a4df74d3a074fe28761f8f129e65
Bruno63-create <julio123coello@gmail.com>
2025-06-28 03:59:48 -0400

    Aqui se muestra la broma a traves del socket, falta hacer que pero no funciona como deberia, me falta arreglarlo

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/*
+
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -11,9 +13,52 @@ app.use(express.static('public'));
 io.on('connection', socket => {
   console.log('Usuario conectado:', socket.id);
 
-  socket.on('mensaje', data => {
+  //socket.on('mensaje', data => {
     // Reenviar a todos menos al que lo envió
+    //socket.broadcast.emit('mensaje', data);
+  //});
+
+  socket.on('propuesta-intercambio', data => {
+  // Reenvía a todos menos al emisor
+  socket.broadcast.emit('propuesta-recibida', data);
+});
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
+
+//////////////////////////////////////////////////////////
+
+//socket.on('mensaje', data => {
+  // (esto era para texto libre, lo mantenemos si lo necesitas)
+//});
+*/
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public')); // Asegúrate de que tu index.html está en /public
+
+// 👇 Todo esto es correcto y debe ir junto
+io.on('connection', socket => {
+  console.log('Usuario conectado:', socket.id);
+
+  socket.on('mensaje', data => {
     socket.broadcast.emit('mensaje', data);
+  });
+
+  socket.on('propuesta-intercambio', data => {
+    socket.broadcast.emit('propuesta-recibida', data);
   });
 
   socket.on('disconnect', () => {
